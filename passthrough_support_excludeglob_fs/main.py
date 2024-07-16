6d467ef78de9d5d6eef8c057958e12028d3db064
AntonyDalmiere <46758577+AntonyDalmiere@users.noreply.github.com>
2024-07-16 21:49:57 +0200

    Fix losing permission when creating exluded file in unexlcluded dir

--- passthrough_support_excludeglob_fs/main.py
+++ passthrough_support_excludeglob_fs/main.py
@@ -353,9 +353,56 @@ class PassthroughFS(LoggingMixIn,Operations):
         else:
             raise FuseOSError(errno.ENOENT)
 
+    #path is already passed trhough get_right_path
+    def makedirs(self, path:str, exist_ok=False):
+        """
+        Recursively create directories at the specified path.
+
+        Args:
+            path (str): The path of the directory to be created.
+            exist_ok (bool, optional): If True, no exception will be raised if the directory already exists. Defaults to False.
+        """
+        # Check if path is cache or full
+        current_location: Literal['cache', 'full'] = 'cache' if path.startswith(self.cache_dir) else 'full'
+        
+        # Get the path at the same level in the other location
+        relative_path = path.replace(self.cache_dir, "") if current_location == 'cache' else path.replace(self.root, "")
+        
+        # Split the path into its components
+        parts = relative_path.split(sep=os.sep)
+        current_path = ""
+        
+        for part in parts:
+            if part:  # Skip empty parts
+                # Append part to the current path
+                current_path = os.path.join(current_path, part)
+                
+                # Get the full path if we are in the cache location and vice versa
+                if current_location == 'cache':
+                    os.makedirs(os.path.join(self.cache_dir, current_path), exist_ok=exist_ok)
+                    
+                    # If directory also exists in the full path, copy metadata (chmod and owner)
+                    full_path = os.path.join(self.root, current_path)
+                    if os.path.exists(full_path):
+                        os.chmod(os.path.join(self.cache_dir, current_path), os.stat(full_path).st_mode)
+                        os.chown(os.path.join(self.cache_dir, current_path), os.stat(full_path).st_uid, os.stat(full_path).st_gid)
+                        # Also copy atime and ctime
+                        os.utime(os.path.join(self.cache_dir, current_path), (os.stat(full_path).st_atime, os.stat(full_path).st_ctime)) 
+                else:  # current_location == 'full'
+                    os.makedirs(os.path.join(self.root, current_path), exist_ok=exist_ok)
+                    
+                    # If directory also exists in the cache path, copy metadata (chmod and owner)
+                    cache_path = os.path.join(self.cache_dir, current_path)
+                    if os.path.exists(cache_path):
+                        os.chmod(os.path.join(self.root, current_path), os.stat(cache_path).st_mode)
+                        os.chown(os.path.join(self.root, current_path), os.stat(cache_path).st_uid, os.stat(cache_path).st_gid)
+                        # Also copy atime and ctime
+                        os.utime(os.path.join(self.root, current_path), (os.stat(cache_path).st_atime, os.stat(cache_path).st_ctime))
+
+
     def create(self, path, mode):
         right_path = self.get_right_path(path)
-        os.makedirs(os.path.dirname(right_path), exist_ok=True)
+        self.makedirs(os.path.dirname(right_path), exist_ok=True)
         fd = os.open(right_path, os.O_RDWR | os.O_CREAT, mode)
         new_fd_id = max(self.file_handles.keys()) + 1 if self.file_handles else 0
         self.file_handles[new_fd_id] = FileHandle(right_path, fd)
