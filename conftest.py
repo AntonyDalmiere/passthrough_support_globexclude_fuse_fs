7dad6e2464cdc824dfa2759d0309f7bf0e987c28
AntonyDalmiere <46758577+AntonyDalmiere@users.noreply.github.com>
2024-07-25 19:45:01 +0200

    Remove useless file

--- conftest.py
+++
@@ -1,9 +0,0 @@
-# import platform
-# from typing import List
-
-# def pytest_configure(config) -> None:
-#     required_workers_count = 4 if platform.system() == 'Windows' else 12
-#     config.option.dist = 'loadgroup'
-#     config.option.numprocesses= required_workers_count
-#     config.option.cov_source = ['passthrough_support_excludeglob_fs']
-#     print(config.option)
