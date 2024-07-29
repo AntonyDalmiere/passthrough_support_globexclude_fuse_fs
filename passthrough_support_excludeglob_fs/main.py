1fc43524015b1128a97730694510f3026262bdec
AntonyDalmiere <46758577+AntonyDalmiere@users.noreply.github.com>
2024-07-30 00:19:45 +0200

    Fix : Now forwading  inode number

--- passthrough_support_excludeglob_fs/main.py
+++ passthrough_support_excludeglob_fs/main.py
@@ -97,7 +97,7 @@ class PassthroughFS(LoggingMixIn,Operations):
         #Edit st to make user RWX perm
         st_dict = dict((key, getattr(st, key,0)) for key in (
             'st_atime', 'st_ctime', 'st_gid', 'st_mtime',
-            'st_nlink', 'st_size', 'st_uid','st_mode','st_birthtime'))
+            'st_nlink', 'st_size', 'st_uid','st_mode','st_birthtime','st_ino','st_dev'))
         #add st_birthtime to st_dict
         if os.name == 'nt':
             st_dict['st_mode'] = st_dict['st_mode'] | 0o777
