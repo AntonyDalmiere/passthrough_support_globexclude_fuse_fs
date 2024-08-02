f23b6d6c81ad7eaf9c0a38ebfcef67b82579d720
AntonyDalmiere <46758577+AntonyDalmiere@users.noreply.github.com>
2024-08-02 12:05:50 +0200

    Refactor cli test

--- test_fscli.py
+++ test_fscli.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pytest
 import os
 import shutil
@@ -5,28 +6,62 @@ import subprocess
 import tempfile
 import time
 import pytest
+from test_fs import determine_mountdir_based_on_os
 
+def clean_up(root_dir: str | None, mountpoint_dir: str | None, process: subprocess.Popen, cache_dir: str | None = None):
+    try:
+        process.kill()
+        time.sleep(0.5)
+    except Exception:
+        pass
 
+    if root_dir and os.path.exists(root_dir):
+        try:
+            shutil.rmtree(root_dir)
+        except Exception:
+            pass
+
+    if mountpoint_dir and os.path.exists(mountpoint_dir):
+        try:
+            shutil.rmtree(mountpoint_dir)
+        except Exception:
+            pass
+
+    if cache_dir and os.path.exists(cache_dir):
+        try:
+            shutil.rmtree(cache_dir)
+        except Exception:
+            pass
 @pytest.fixture
-def virtual_env(tmp_path):
+def virtual_env(tmp_path) -> Path:
     """Create a virtual environment for testing."""
     venv_dir = tmp_path / "venv"
     subprocess.check_call(["python3", "-m", "venv", str(venv_dir)])
-    subprocess.check_call([str(venv_dir / "bin" / "pip"), "install", "."])
+    if os.name == 'nt':
+        subprocess.check_call([str(venv_dir / "Scripts" / "pip"), "install", "."])
+    else:
+        subprocess.check_call([str(venv_dir / "bin" / "pip"), "install", "."])
     return venv_dir
 
+@pytest.fixture
+def command_location(virtual_env)-> Path:
+    """Get the location of commands sych as passthrough_support_excludeglob_fs or pip or python."""
+    if os.name == 'nt':
+        return virtual_env / "Scripts"
+    else:
+        return virtual_env / "bin"
 @pytest.mark.xdist_group(name="cli")
 @pytest.mark.parametrize("complementary_option",['',',patterns=**/*.txt',',patterns=**/*.txt:**/exc/*'])
-def test_cli(virtual_env,complementary_option):
+def test_cli(command_location: Path,complementary_option):
     """Test the CLI of the filesystem."""
 
     # Create temporary directories for root and mountpoint
     root_dir = tempfile.mkdtemp()
-    mountpoint_dir = tempfile.mkdtemp()
+    mountpoint_dir = determine_mountdir_based_on_os()
 
     # Run the CLI command in a separate process
     cli_command = [
-        str(virtual_env / "bin" / "passthrough_support_excludeglob_fs"),
+        str(command_location / "passthrough_support_excludeglob_fs"),
         mountpoint_dir,
         "-o",
         f"root={root_dir}{complementary_option}",
@@ -90,26 +125,22 @@ def test_cli(virtual_env,complementary_option):
     else:
         subprocess.check_call(["fusermount", "-u", mountpoint_dir])
 
-    # Clean up
-    process.terminate()
-    shutil.rmtree(root_dir)
-    shutil.rmtree(mountpoint_dir)
-
+    clean_up(root_dir=root_dir, mountpoint_dir=mountpoint_dir, process=process)
 
 #Same as above but will manually specify cache_dir to also cheack each time the presence of excluded dir in it
 @pytest.mark.xdist_group(name="cli")
 @pytest.mark.parametrize("complementary_option",['',',patterns=**/*.txt',',patterns=**/*.txt:**/exc/*',',patterns=**/*.txt:**/exc/*'])
-def test_cli_cache(virtual_env,complementary_option):
+def test_cli_cache(command_location,complementary_option):
     """Test the CLI of the filesystem."""
 
     # Create temporary directories for root and mountpoint
     root_dir = tempfile.mkdtemp()
-    mountpoint_dir = tempfile.mkdtemp()
+    mountpoint_dir = determine_mountdir_based_on_os()
     cache_dir = tempfile.mkdtemp()
 
     # Run the CLI command in a separate process
     cli_command = [
-        str(virtual_env / "bin" / "passthrough_support_excludeglob_fs"),
+        str(command_location / "passthrough_support_excludeglob_fs"),
         mountpoint_dir,
         "-o",
         f"root={root_dir},cache_dir={cache_dir}{complementary_option}",
@@ -173,29 +204,43 @@ def test_cli_cache(virtual_env,complementary_option):
     else:
         subprocess.check_call(["fusermount", "-u", mountpoint_dir])
 
-    # Clean up
-    process.terminate()
-    shutil.rmtree(root_dir)
-    shutil.rmtree(mountpoint_dir)
-    shutil.rmtree(cache_dir)
+    clean_up(root_dir, mountpoint_dir, process, cache_dir)
 
 #Same as above but will use capsys to check the stdout against  the debug=False option
 @pytest.mark.xdist_group(name="cli")
 @pytest.mark.parametrize("debug", [True, False])
-def test_cli_debug(virtual_env, debug):
+def test_cli_debug(command_location, debug):
     """Test the CLI of the filesystem."""
+    import threading
 
     # Create temporary directories for root and mountpoint
     root_dir = tempfile.mkdtemp()
-    mountpoint_dir = tempfile.mkdtemp()
+    mountpoint_dir = determine_mountdir_based_on_os()
 
     # Run the CLI command in a separate process
     cli_command = [
-        str(virtual_env / "bin" / "passthrough_support_excludeglob_fs"),
+        str(command_location / "passthrough_support_excludeglob_fs"),
         mountpoint_dir,
         "-o",
         f"root={root_dir},debug={debug}"]
-    process = subprocess.Popen(cli_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    process = subprocess.Popen(cli_command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=1,shell=False,text=True)
+
+    #Its a bit hacky but retrieving the stdout from the process with communicate or stdout.read* will cause a deadlock.
+    class StdoutReader(threading.Thread):
+        def __init__(self, process):
+            threading.Thread.__init__(self)
+            self.process = process
+            self.stdout = ''
+            self.daemon = True  # Daemonize the thread
+        def run(self):
+            for line in iter(self.process.stdout.readline, b''):
+                self.stdout += line
+
+    stdout_reader = StdoutReader(process)
+    stdout_reader.start()
+
+
 
     # Wait for the filesystem to mount
     time.sleep(2)
@@ -227,29 +272,26 @@ def test_cli_debug(virtual_env, debug):
     else:
         subprocess.check_call(["fusermount", "-u", mountpoint_dir])
 
-    # Clean up
-    process.terminate()
-    shutil.rmtree(root_dir)
-    shutil.rmtree(mountpoint_dir)
 
-    out, err = process.communicate()
+    clean_up(root_dir=root_dir, mountpoint_dir=mountpoint_dir, process=process)
+    out = stdout_reader.stdout
     if debug:
-        assert b'Verbose mode enabled' in out
+        assert 'Verbose mode enabled' in out
     else:
-        assert b'Verbose mode enabled' not in out
+        assert 'Verbose mode enabled' not in out
 
 @pytest.mark.xdist_group(name="cli")
-def test_cli_escaping_patterns(virtual_env):
+def test_cli_escaping_patterns(command_location):
     """Test the CLI of the filesystem with escaping."""
 
     # Create temporary directories for root and mountpoint
     root_dir = tempfile.mkdtemp()
-    mountpoint_dir = tempfile.mkdtemp()
+    mountpoint_dir = determine_mountdir_based_on_os()
     cache_dir = tempfile.mkdtemp()
 
     # Run the CLI command in a separate process with escaping
     cli_command = [
-        str(virtual_env / "bin" / "passthrough_support_excludeglob_fs"),
+        str(command_location / "passthrough_support_excludeglob_fs"),
         mountpoint_dir,
         "-o",
         f"root={root_dir},cache_dir={cache_dir},patterns=**/\\,test\\=file.txt:**/\\ exc\\ dir/*"
@@ -298,14 +340,10 @@ def test_cli_escaping_patterns(virtual_env):
     else:
         subprocess.check_call(["fusermount", "-u", mountpoint_dir])
 
-    # Clean up
-    process.terminate()
-    shutil.rmtree(root_dir)
-    shutil.rmtree(mountpoint_dir)
-    shutil.rmtree(cache_dir)
+    clean_up(root_dir=root_dir, mountpoint_dir=mountpoint_dir, process=process, cache_dir=cache_dir)
 
 @pytest.mark.xdist_group(name="cli")
-def test_cli_escaping_mountdir(virtual_env):
+def test_cli_escaping_mountdir(command_location):
     """Test the CLI of the filesystem with escaping in mountdir."""
 
     # Create temporary directories for root and mountpoint
@@ -314,7 +352,7 @@ def test_cli_escaping_mountdir(virtual_env):
 
     # Run the CLI command in a separate process with escaping
     cli_command = [
-        str(virtual_env / "bin" / "passthrough_support_excludeglob_fs"),
+        str(command_location / "passthrough_support_excludeglob_fs"),
         mountpoint_dir,
         "-o",
         f"root={root_dir}"
@@ -339,24 +377,21 @@ def test_cli_escaping_mountdir(virtual_env):
     else:
         subprocess.check_call(["fusermount", "-u", mountpoint_dir])
 
-    # Clean up
-    process.terminate()
-    shutil.rmtree(root_dir)
-    shutil.rmtree(mountpoint_dir)
+    clean_up(root_dir=root_dir, mountpoint_dir=mountpoint_dir, process=process)
 
 @pytest.mark.xdist_group(name="cli")
-def test_cli_escaping_rootdir(virtual_env):
+def test_cli_escaping_rootdir(command_location):
     """Test the CLI of the filesystem with escaping in rootdir."""
 
     # Create temporary directories for root and mountpoint
     root_dir = tempfile.mkdtemp(prefix="root\\,dir\\=")
-    mountpoint_dir = tempfile.mkdtemp()
+    mountpoint_dir = determine_mountdir_based_on_os()
 
     print(f'root_dir: {root_dir}')
 
     # Run the CLI command in a separate process with escaping
     cli_command = [
-        str(virtual_env / "bin" / "passthrough_support_excludeglob_fs"),
+        str(command_location / "passthrough_support_excludeglob_fs"),
         mountpoint_dir,
         "-o",
         f"root={root_dir}"
@@ -381,18 +416,15 @@ def test_cli_escaping_rootdir(virtual_env):
     else:
         subprocess.check_call(["fusermount", "-u", mountpoint_dir])
 
-    # Clean up
-    process.terminate()
-    shutil.rmtree(root_dir)
-    shutil.rmtree(mountpoint_dir)
+    clean_up(root_dir=root_dir, mountpoint_dir=mountpoint_dir, process=process)
 
 @pytest.mark.xdist_group(name="cli")
-def test_cli_escaping_cachedir(virtual_env):
+def test_cli_escaping_cachedir(command_location):
     """Test the CLI of the filesystem with escaping in cachedir."""
 
     # Create temporary directories for root and mountpoint
     root_dir = tempfile.mkdtemp()
-    mountpoint_dir = tempfile.mkdtemp()
+    mountpoint_dir = determine_mountdir_based_on_os()
     cache_dir = tempfile.mkdtemp(prefix="cache\\,dir\\=")
 
     print(f'cache_dir: {cache_dir}')
@@ -400,7 +432,7 @@ def test_cli_escaping_cachedir(virtual_env):
 
     # Run the CLI command in a separate process with escaping
     cli_command = [
-        str(virtual_env / "bin" / "passthrough_support_excludeglob_fs"),
+        str(command_location / "bin" / "passthrough_support_excludeglob_fs"),
         mountpoint_dir,
         "-o",
         f"root={root_dir},cache_dir={cache_dir}"
@@ -425,8 +457,5 @@ def test_cli_escaping_cachedir(virtual_env):
     else:
         subprocess.check_call(["fusermount", "-u", mountpoint_dir])
 
-    # Clean up
-    process.terminate()
-    shutil.rmtree(root_dir)
-    shutil.rmtree(mountpoint_dir)
-    shutil.rmtree(cache_dir)
+    clean_up(root_dir=root_dir, mountpoint_dir=mountpoint_dir, process=process, cache_dir=cache_dir)
+
