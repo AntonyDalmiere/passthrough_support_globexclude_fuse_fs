58c672bc1d85151f5ece1ed11a689c534669381b
AntonyDalmiere <46758577+AntonyDalmiere@users.noreply.github.com>
2024-09-12 21:51:00 +0200

    refactor: Improve handling of file deletion in unlink_operation

--- passthrough_support_excludeglob_fs/fs_operations/unlink_operation.py
+++ passthrough_support_excludeglob_fs/fs_operations/unlink_operation.py
@@ -1,15 +1,14 @@
 import os
 import errno
 from refuse.high import FuseOSError
-
+if os.name == 'nt':
+    from ctypes import windll
 def unlink_operation(self, path):
     right_path = self.get_right_path(path)
 
     if os.path.lexists(right_path):
         if os.name == 'nt':
-            #Use native Ctype Win32 API to delete file
-            from ctypes import windll
-            windll.kernel32.DeleteFileW(right_path)
+            return windll.kernel32.DeleteFileW(right_path)
         else:
             return os.unlink(right_path)
     raise FuseOSError(errno.ENOENT)
